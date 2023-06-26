f4c57c6310905c84a4220618cb6548b4d60bc640
Navya Varda <navya.varda@gmail.com>
2023-06-26 15:27:50 -0400

    Initial Commit

---
+++ rand/randApp.swift
@@ -0,0 +1,17 @@
+//
+//  randApp.swift
+//  rand
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct randApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
